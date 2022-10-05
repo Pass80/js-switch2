8b7b1d370cd9850e63140a0de83e51031af3f1d3
Feras <ferassaleh1980@gmail.com>
2022-10-05 20:40:14 +0200

    js übung

---
+++ main.js
@@ -0,0 +1,75 @@
+const inputValue = document.getElementById('bundeslandInfo');
+const text = document.getElementById('bundeslandInfoErgebnis');
+
+const check = () => {
+    switch (inputValue.value) {
+        case 'Baden-Württemberg':
+            text.innerHTML =
+                'Baden-Württemberg hat 10,880 Mio Einwohner und Stuttgart ist die Hauptstadt';
+            break;
+        case 'Bayern':
+            text.innerHTML =
+                'Bayern hat 12,844 Mio Einwohner und München ist die Hauptstadt';
+            break;
+        case 'Berlin':
+            text.innerHTML =
+                'Berlin hat 3,520 Mio Einwohner und Berlin ist die Hauptstadt';
+            break;
+        case 'Brandenburg':
+            text.innerHTML =
+                'Brandenburg hat 2,485 Mio Einwohner und Potsdam ist die Hauptstadt';
+            break;
+        case 'Bremen':
+            text.innerHTML =
+                'Bremen hat 0,671 Mio Einwohner und Bremen ist die Hauptstadt';
+            break;
+        case 'Hamburg':
+            text.innerHTML =
+                'Hamburg hat 1,787 Mio Einwohner und Hamburg ist die Hauptstadt';
+            break;
+        case 'Hessen':
+            text.innerHTML =
+                'Hessen hat 6,176 Mio Einwohner und Wiesbaden ist die Hauptstadt';
+            break;
+        case 'Mecklenburg-Vorpommern':
+            text.innerHTML =
+                'Mecklenburg-Vorpommern hat 1,612 Mio Einwohner und Schwerin ist die Hauptstadt';
+            break;
+        case 'Niedersachsen':
+            text.innerHTML =
+                'Niedersachsen hat 7,927 Mio Einwohner und Hannover ist die Hauptstadt';
+            break;
+        case 'Nordrhein-Westfalen':
+            text.innerHTML =
+                'Nordrhein-Westfalen hat 17,865 Mio Einwohner und Düsseldorf ist die Hauptstadt';
+            break;
+        case 'Rheinland-Pfalz':
+            text.innerHTML =
+                'Rheinland-Pfalz hat 4,053 Mio Einwohner und Mainz ist die Hauptstadt';
+            break;
+        case 'Saarland':
+            text.innerHTML =
+                'Saarland hat 0,996 Mio Einwohner und Saarbrücken ist die Hauptstadt';
+            break;
+        case 'Sachsen':
+            text.innerHTML =
+                'Sachsen hat 4,085 Mio Einwohner und Dresden ist die Hauptstadt';
+            break;
+        case 'Sachsen-Anhalt':
+            text.innerHTML =
+                'Sachsen-Anhalt hat 2,245 Mio Einwohner und Magdeburg ist die Hauptstadt';
+            break;
+        case 'Schleswig-Holstein':
+            text.innerHTML =
+                'Schleswig-Holstein hat 2,859 Mio Einwohner und Kiel ist die Hauptstadt';
+            break;
+        case 'Thüringen':
+            text.innerHTML =
+                'Thüringen hat 2,171 Mio Einwohner und Erfurt ist die Hauptstadt';
+            break;
+        default:
+            text.innerHTML =
+                'Ein solches Bundesland gibt es in Deutschland nicht.';
+            break;
+    }
+};
